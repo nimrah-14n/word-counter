c9153b4fbfe5110962e747caa8f1c90354ded3d3
unknown <mehreenk433@gmail.com>
2024-06-29 10:43:02 -0700

    word counter

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 #! /usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var inquirer_1 = require("inquirer");
-var answers = await inquirer_1.default.prompt([
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
     {
         name: "sentence",
         message: "Enter your sentence to count the word:",
         type: "input"
     }
 ]);
-var words = answers.sentence.trim().split(" ");
+const words = answers.sentence.trim().split(" ");
 console.log(words);
-console.log("Your sentence word count is ".concat(words.length));
+console.log(`Your sentence word count is ${words.length}`);
